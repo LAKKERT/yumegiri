34098420808fed5335b4696ae1ebb635a7eefc62
LAKKERT <lakkert2002@yandex.ru>
2026-04-19 20:33:44 +0500

    refactor: remove variants and simplify motions

--- src/app/components/restaurant/mainInformation.tsx
+++ src/app/components/restaurant/mainInformation.tsx
@@ -4,100 +4,125 @@ import { Places } from "@/lib/interfaces/mockup";
 import { motion, AnimatePresence, type MotionValue } from "framer-motion";
 import Image from "next/image";
 import { RefObject } from "react";
-import { navigateButton } from "@/lib/variants/restaurant";
 
 interface MainInfo {
-    prevImageHandler: () => void,
-    nextImageHandler: () => void,
-    carouselRef: RefObject<HTMLDivElement | null>,
-    maskImage: MotionValue<string>,
-    isLastImage: boolean,
-    currentRestaurant: Places | undefined,
-    order: number,
+    prevImageHandler: () => void;
+    nextImageHandler: () => void;
+    carouselRef: RefObject<HTMLDivElement | null>;
+    maskImage: MotionValue<string>;
+    isLastImage: boolean;
+    currentRestaurant: Places | undefined;
+    order: number;
 }
 
-export function MainInfo({ prevImageHandler, nextImageHandler, carouselRef, maskImage, isLastImage, currentRestaurant, order }: MainInfo) {
+export function MainInfo({
+    prevImageHandler,
+    nextImageHandler,
+    carouselRef,
+    maskImage,
+    isLastImage,
+    currentRestaurant,
+    order
+}: MainInfo) {
     return (
         <div className="flex flex-row items-center gap-4">
+
+            {/* LEFT BUTTON */}
             <motion.button
                 type="button"
                 className="w-6.25 h-6.25 bg-white rounded-full text-black cursor-pointer"
                 onClick={prevImageHandler}
-
-                variants={navigateButton}
-                initial='init'
-                whileTap='tap'
+                whileTap={{
+                    scale: 0.8,
+                    transition: {
+                        duration: 0.2,
+                        ease: "easeInOut",
+                    },
+                }}
+                transition={{
+                    duration: 0.3,
+                    ease: "easeInOut",
+                }}
             >
                 <Image
-                    src={'/other/arrow.svg'}
+                    src={"/other/arrow.svg"}
                     alt="previous"
                     quality={100}
                     width={25}
                     height={25}
                 />
             </motion.button>
-            <motion.div ref={carouselRef}
+
+            {/* CAROUSEL */}
+            <motion.div
+                ref={carouselRef}
                 style={{
                     maskImage,
-                    paddingRight: isLastImage ? "" : `${(200 * 2)}px`,
-                    width: isLastImage ? `250px` : `650px`
+                    paddingRight: isLastImage ? 0 : "400px",
+                    width: isLastImage ? "250px" : "650px",
                 }}
-                className={`snap-x scroll-smooth flex flex-row items-center gap-4 overflow-hidden overflow-x-hidden transform transition-all duration-300`}
+                className="snap-x scroll-smooth flex flex-row items-center gap-4 overflow-hidden overflow-x-hidden transform transition-all duration-300"
             >
-                <AnimatePresence mode='wait'>
-                    {Array.isArray(currentRestaurant?.gallery) ? (
-                        currentRestaurant?.gallery.map((image, index) => (
-                            <motion.div
-                                key={image.id}
-                                id={`image${index}`}
-                                className={`relative snap-start bg-white shrink-0 transform-3d  ${isLastImage ? 'scroll-ml-0' : 'scroll-ml-4'} ${order === index ? 'w-68.75 h-83.75' : 'min-w-62.5 w-62.5 h-77.5'} rounded-md`}
-                                initial={{
-                                    y: 80,
-                                    opacity: 0
-                                }}
-                                exit={{
-                                    y: -80,
-                                    opacity: 0,
-                                    background: '#000'
-                                }}
-
-                                animate={{
-                                    y: 0,
-                                    opacity: 1,
-                                    background: '#000'
-                                }}
-                                transition={{
-                                    duration: .3,
-                                    ease: "easeInOut"
-                                }}
-                            >
-                                <Image
-                                    src={image.image}
-                                    alt="restaurant gallery"
-                                    fill
-                                    className={`rounded-md origin-center`}
-                                >
-
-                                </Image>
-                            </motion.div>
-                        ))
-                    ) : (
-                        null
-                    )}
+                <AnimatePresence mode="wait">
+                    {Array.isArray(currentRestaurant?.gallery)
+                        ? currentRestaurant.gallery.map((image, index) => (
+                              <motion.div
+                                  key={image.id}
+                                  id={`image${index}`}
+                                  className={`relative snap-start bg-white shrink-0 transform-3d ${
+                                      isLastImage ? "scroll-ml-0" : "scroll-ml-4"
+                                  } ${
+                                      order === index
+                                          ? "w-68.75 h-83.75"
+                                          : "min-w-62.5 w-62.5 h-77.5"
+                                  } rounded-md`}
+                                  initial={{
+                                      y: 80,
+                                      opacity: 0,
+                                  }}
+                                  exit={{
+                                      y: -80,
+                                      opacity: 0,
+                                      background: "#000",
+                                  }}
+                                  animate={{
+                                      y: 0,
+                                      opacity: 1,
+                                      background: "#000",
+                                  }}
+                                  transition={{
+                                      duration: 0.3,
+                                      ease: "easeInOut",
+                                  }}
+                              >
+                                  <Image
+                                      src={image.image}
+                                      alt="restaurant gallery"
+                                      fill
+                                      className="rounded-md origin-center object-cover"
+                                  />
+                              </motion.div>
+                          ))
+                        : null}
                 </AnimatePresence>
             </motion.div>
+
+            {/* RIGHT BUTTON */}
             <motion.button
                 type="button"
                 className="w-6.25 h-6.25 bg-white rounded-full text-black cursor-pointer"
                 onClick={nextImageHandler}
-
-                variants={navigateButton}
-                initial='init'
-                whileTap='tap'
+                whileTap={{
+                    scale: 0.8,
+                    transition: {
+                        duration: 0.2,
+                        ease: "easeInOut",
+                    },
+                }}
             >
                 <Image
                     className="rotate-180"
-                    src={'/other/arrow.svg'}
+                    src={"/other/arrow.svg"}
                     alt="previous"
                     quality={100}
                     width={25}
@@ -105,5 +130,5 @@ export function MainInfo({ prevImageHandler, nextImageHandler, carouselRef, mask
                 />
             </motion.button>
         </div>
-    )
+    );
 }
